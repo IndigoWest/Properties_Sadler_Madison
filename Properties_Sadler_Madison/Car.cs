7a6272e4a0c25f39c8e6320eae116a7aed699275
IndigoWest <maddie.sadler2005@gmail.com>
2023-04-17 10:30:53 -0500

    Add project files.

---
+++ Properties_Sadler_Madison/Car.cs
@@ -0,0 +1,35 @@
+﻿// System namespace needed to use Console class.
+using System;
+// System.Collections.Generic namespace needed to use Console class.
+using System.Collections.Generic;
+// System.Text namespace needed to use Console class.
+using System.Text;
+
+// My collection of classes for this project
+namespace Properties_Sadler_Madison
+{
+    // Main class for the program
+    class Car
+    {
+        // Creates a private string called model
+        private string model;
+
+        // Creates a public string called Model
+        public string Model
+        {
+            // Sets the getter
+            get { return model; }
+            // Sets the setter
+            set { model = value; }
+        }
+
+        // Creates a public string called Make
+        public string Make
+        {
+            // Sets the getter
+            get;
+            // Sets the setter
+            set;
+        }
+    }
+}
---
+++ Properties_Sadler_Madison/Program.cs
@@ -0,0 +1,34 @@
+﻿// System namespace needed to use Console class.
+using System;
+
+// My collection of classes for this project.
+namespace Properties_Sadler_Madison
+{
+    // Main class for the program
+    class Program
+    {
+        // The Main method is executed when the application starts
+        static void Main(string[] args)
+        {
+            // Initializes new Car() under Car car
+            Car car = new Car();
+            // Assigns car.Make to Chevy
+            car.Make = "Chevy";
+            // Assigns car.Model to Trax
+            car.Model = "Trax";
+
+            // Prints to the console the make and model of car
+            Console.WriteLine($"Make: {car.Make}, Model: {car.Model}");
+
+            // Initializes new Car() under Car car2
+            Car car2 = new Car();
+            // Assigns car2.Make to Ford
+            car2.Make = "Ford";
+            // Assigns car2.Model to Fusion
+            car2.Model = "Fusion";
+
+            // Prints to the console the make and model of car2
+            Console.WriteLine($"Make: {car2.Make}, Model: {car2.Model}");
+        }
+    }
+}
